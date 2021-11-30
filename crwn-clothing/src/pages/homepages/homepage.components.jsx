38de5c6142c148aa942c51c084da2c1fdbced10b
tarunbasak007 <tarunbasak007@gmail.com>
2021-11-30 20:36:34 +0530

    broken our home component, styling our menu component done

---
+++ crwn-clothing/src/pages/homepages/homepage.components.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Directory from "../../components/directory/directory.component";
+// from "../../components/menu-item/menu-item.component";
+import "./homepage.styles.scss";
+
+const HomePage = () => (
+  <div className="homepage">
+    <Directory />
+  </div>
+);
+
+export default HomePage;
